125158bab7c5537beedb135c52219903dc54853a
Kyrsantik <kurskijartem555@gmail.com>
2024-10-08 15:10:14 +0300

    Подправленный RUN.py

--- RUN.py
+++ RUN.py
@@ -8,13 +8,13 @@ import sqlite3
 
 
 
-bot = telebot.TeleBot("7341944123:AAG5BOXZY-0KFMdYuML_m1_gssMZvDwj-88")
+bot = telebot.TeleBot("your_token")
 
 
 
 
 @bot.message_handler(commands=['start'])
-def start1(message):
+def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Четная")
     btn2 = types.KeyboardButton("Нечетная")
@@ -26,16 +26,15 @@ def start1(message):
 
 
 @bot.message_handler(content_types=['text'])
-#@bot.message_handler(func=lambda message: message.text == "Нечетная")
-def send_hi(message):
+def week_sending(message):
 	if (message.text == "Нечетная"):
 		bot.send_message(message.chat.id, text="Выбирай день")
 		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-		btn1 = types.KeyboardButton("1Понедельник")
-		btn2 = types.KeyboardButton("1Вторник")
-		btn3 = types.KeyboardButton("1Среда")
-		btn4 = types.KeyboardButton("1Четверг")
-		btn5 = types.KeyboardButton("1Пятница")
+		btn1 = types.KeyboardButton(".Понедельник")
+		btn2 = types.KeyboardButton(".Вторник.")
+		btn3 = types.KeyboardButton(".Среда.")
+		btn4 = types.KeyboardButton(".Четверг.")
+		btn5 = types.KeyboardButton(".Пятница.")
 		back = types.KeyboardButton("Назад")
 		markup.add(btn1, btn2, btn3,btn4,btn5,back)
 		bot.send_message(message.chat.id,text="//////////////////",reply_markup=markup)
@@ -53,7 +52,7 @@ def send_hi(message):
 		conn.close()
 		bot.send_message(message.chat.id, response)
 
-	elif message.text == "1Вторник":
+	elif message.text == ".Вторник.":
 		conn = sqlite3.connect('nemmy_database.db')
 		cursor = conn.cursor()
 
@@ -66,7 +65,7 @@ def send_hi(message):
 
 		conn.close()
 		bot.send_message(message.chat.id, response)
-	elif message.text == "1Среда":
+	elif message.text == ".Среда.":
 		conn = sqlite3.connect('nemmy_database.db')
 		cursor = conn.cursor()
 
@@ -79,7 +78,7 @@ def send_hi(message):
 
 		conn.close()
 		bot.send_message(message.chat.id, response)
-	elif message.text == "1Четверг":
+	elif message.text == ".Четверг.":
 		conn = sqlite3.connect('nemmy_database.db')
 		cursor = conn.cursor()
 
@@ -92,7 +91,7 @@ def send_hi(message):
 
 		conn.close()
 		bot.send_message(message.chat.id, response)
-	elif message.text == "1Пятница":
+	elif message.text == ".Пятница.":
 		conn = sqlite3.connect('nemmy_database.db')
 		cursor = conn.cursor()
 
@@ -207,136 +206,6 @@ def send_hi(message):
 
 		bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
 
-
-
-
-
-
-
-	elif (message.text == "Назад"):
-		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-		button1 = types.KeyboardButton("Четная")
-		button2 = types.KeyboardButton("Нечетная")
-		markup.add(button1, button2)
-		bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
-
-
-
-
-
-
-
-#@bot.message_handler(content_types=['text'])
-@bot.message_handler(content_types=['text'])
-def s1end_hi1(message):
-	if (message.text == "Четная"):
-		bot.send_message(message.chat.id, text="1Выбирай день1")
-		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-		btn11 = types.KeyboardButton("Понедельник")
-		btn22 = types.KeyboardButton("Вторник")
-		btn33 = types.KeyboardButton("Среда")
-		btn44 = types.KeyboardButton("Четверг")
-		btn55 = types.KeyboardButton("Пятница")
-		back1 = types.KeyboardButton("Назад")
-		markup.add(btn11, btn22, btn33,btn44,btn55,back1)
-		bot.send_message(message.chat.id, text="None", reply_markup=markup)
-	if (message.text == "Понедельник"):
-		conn = sqlite3.connect('my_database.db')
-		cursor = conn.cursor()
-
-		cursor.execute("SELECT * FROM Понедельник")
-		rows = cursor.fetchall()
-
-		response = "Понедельник:\n\n"
-		for row in rows:
-			response += f"Пара: {row[0]},  {row[1]},  {row[2]}, {row[3]}\n"
-
-		conn.close()
-		bot.send_message(message.chat.id, response)
-
-
-
-	elif (message.text == "Понедельник"):
-		conn = sqlite3.connect('my_database.db')
-		cursor = conn.cursor()
-
-		cursor.execute("SELECT * FROM Понедельник")
-		rows = cursor.fetchall()
-
-		response = "Понедельник:\n\n"
-		for row in rows:
-			response += f"Пара: {row[0]},  {row[1]},  {row[2]}, {row[3]}\n"
-
-		conn.close()
-		bot.send_message(message.chat.id, response)
-
-	elif message.text == "Вторник":
-		conn = sqlite3.connect('my_database.db')
-		cursor = conn.cursor()
-
-		cursor.execute("SELECT * FROM Вторник")
-		rows = cursor.fetchall()
-
-		response = "Вторник:\n\n"
-		for row in rows:
-			response += f"Пара: {row[0]},  {row[1]},  {row[2]}, {row[3]}\n"
-
-		conn.close()
-		bot.send_message(message.chat.id, response)
-	elif message.text == "Среда":
-		conn = sqlite3.connect('my_database.db')
-		cursor = conn.cursor()
-
-		cursor.execute("SELECT * FROM Среда")
-		rows = cursor.fetchall()
-
-		response = "Среда:\n\n"
-		for row in rows:
-			response += f"Пара: {row[0]},  {row[1]},  {row[2]}, {row[3]}\n"
-
-		conn.close()
-		bot.send_message(message.chat.id, response)
-	elif message.text == "Четверг":
-		conn = sqlite3.connect('my_database.db')
-		cursor = conn.cursor()
-
-		cursor.execute("SELECT * FROM Четверг")
-		rows = cursor.fetchall()
-
-		response = "Четверг:\n\n"
-		for row in rows:
-			response += f"Пара: {row[0]},  {row[1]},  {row[2]}, {row[3]}\n"
-
-		conn.close()
-		bot.send_message(message.chat.id, response)
-	elif message.text == "Пятница":
-		conn = sqlite3.connect('my_database.db')
-		cursor = conn.cursor()
-
-		cursor.execute("SELECT * FROM Пятница")
-		rows = cursor.fetchall()
-
-		response = "Пятница:\n\n"
-		for row in rows:
-			response += f"Пара: {row[0]},  {row[1]},  {row[2]}, {row[3]}\n"
-
-		conn.close()
-		bot.send_message(message.chat.id, response)
-
-
-	elif (message.text == "Назад"):
-
-		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-
-		button1 = types.KeyboardButton("Четная")
-
-		button2 = types.KeyboardButton("Нечетная")
-
-		markup.add(button1, button2)
-
-		bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
-
-		s1end_hi1(message)
 
 
 
